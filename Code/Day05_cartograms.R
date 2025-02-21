63143dccca0af8bab4c29605055adaf4e798aa53
Duccio Rocchini <ducciorocchini@gmail.com>
2025-02-21 17:12:50 +0100

    Update Day05_cartograms.R

--- Code/Day05_cartograms.R
+++ Code/Day05_cartograms.R
@@ -15,10 +15,29 @@ worldn <- st_transform(World, 3395)
 afr_cont <- cartogram_cont(afr, "pop_est", itermax = 5)
 worldn_cont <- cartogram_cont(worldn, "pop_est", itermax = 5)
 
-
 # plot it
 tm_shape(afr_cont) + tm_polygons("pop_est", style = "jenks") +
   tm_layout(frame = FALSE, legend.position = c("left", "bottom"))
 
 tm_shape(worldn_cont) + tm_polygons("pop_est", style = "jenks") +  
   tm_layout(frame = FALSE, legend.position = c("left", "bottom"))
+
+#------------
+# Additional potential analysis of interest out of the course:
+
+# Otherwise: distort on population and color as footprint
+tm_shape(worldn_cont) + tm_polygons("footprint", style = "jenks") +  
+  tm_layout(frame = FALSE, legend.position = c("left", "bottom"))
+
+# Footprint genefal:
+worldn_cont <- cartogram_cont(worldn, "footprint", itermax = 5)
+tm_shape(worldn_cont) + tm_polygons("footprint", style = "jenks") +  
+  tm_layout(frame = FALSE, legend.position = c("left", "bottom"))
+
+# Wirth style=pretty with the footprint you will see the two north south parts of the planet 
+tm_shape(worldn_cont) + tm_polygons("footprint", style = "pretty") +  
+  tm_layout(frame = FALSE, legend.position = c("left", "bottom"))
+
+
+
+
